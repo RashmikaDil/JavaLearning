c0de68166d05363140916bee6b01a1001387da2c
Rashmika Dillhara <rashmikadil2023@gmail.com>
2025-09-29 09:59:58 +0530

    Initial commit

---
+++ src/Single_linked_list.java
@@ -0,0 +1,10 @@
+public class Single_linked_list {
+    class Node{
+        int data;
+        Node Next;
+        Node(int data){
+            this.data = data;
+            this.Next= null;
+        }
+    }
+}
